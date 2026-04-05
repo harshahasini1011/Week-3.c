05308116ab8b40ee725df4ddc2d398cfb1a74eaa
harshahasini1011 <harshahasini1011@gmail.com>
2026-04-05 11:59:53 +0530

    Add Experiment 3.c for grade calculation

---
+++ Experiment 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int marks;
+printf("enter marks:");
+scanf("%d",&marks);
+if(marks>=90)
+printf("grade A");
+else if(marks>=70)
+printf("grade=B");
+else if(marks>=50)
+printf("grade=C");
+else
+printf("fail");
+return 0;
+}
